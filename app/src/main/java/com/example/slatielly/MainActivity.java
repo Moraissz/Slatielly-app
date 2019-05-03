8bd16770ff46a377c49c3691e074f7331a12acec
Lucas Morais <lucas.moraissz07@gmail.com>
2019-05-03 15:39:38 -0300

    add: Adding the initial dress list

--- app/src/main/java/com/example/slatielly/MainActivity.java
+++ app/src/main/java/com/example/slatielly/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.slatielly;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.slatielly.Model.Dress;
+import com.example.slatielly.view.DressAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements DressAdapter.DressListener {
+    private RecyclerView recyclerView;
+    private DressAdapter adapter;
+    private ArrayList<Dress> dressArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.globalRecyclerView);
+        dressArrayList = new ArrayList<>();
+        //Preencher array
+        for(int i = 0; i< 20;i++){
+            Dress dress = new Dress(i,"Vestido para Casamento","Tipo "+i,"R$ 50,00");
+            dressArrayList.add(dress);
+        }
+        adapter = new DressAdapter(dressArrayList,this);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClickDressListener(Dress dress) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
---
+++ app/src/main/java/com/example/slatielly/Model/Dress.java
@@ -0,0 +1,50 @@
+package com.example.slatielly.Model;
+
+import android.graphics.Bitmap;
+
+public class Dress {
+    private int id;
+    private String description;
+    private String type;
+    private String price;
+
+
+    public Dress(int id, String description, String type, String price) {
+        this.id = id;
+        this.description = description;
+        this.type = type;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
