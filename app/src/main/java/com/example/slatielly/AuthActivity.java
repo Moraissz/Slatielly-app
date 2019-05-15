71bd73f2a3bebdb040d24782b73425188660707f
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-15 19:08:11 -0300

    redirect to MainActivity when user is already logged in

--- app/src/main/java/com/example/slatielly/AuthActivity.java
+++ app/src/main/java/com/example/slatielly/AuthActivity.java
@@ -5,11 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            this.startActivity(intent);
+        }
     }
 
     public void onClickJoin(View v) {
