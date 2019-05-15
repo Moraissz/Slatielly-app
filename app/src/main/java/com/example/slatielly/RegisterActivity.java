3f42e564fdc61aa518135cac0fc2e214c5fe75c7
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-15 11:21:53 -0300

    register user with email and passwordi in firebase

--- app/src/main/java/com/example/slatielly/RegisterActivity.java
+++ app/src/main/java/com/example/slatielly/RegisterActivity.java
@@ -1,13 +1,52 @@
 package com.example.slatielly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements OnCompleteListener<AuthResult> {
+    private EditText ptxtEmail;
+    private EditText ptxtPassword;
+    private ProgressBar loadingBar;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.ptxtEmail = this.findViewById(R.id.ptxtEmail);
+        this.ptxtPassword = this.findViewById(R.id.ptxtPassword);
+        this.loadingBar = this.findViewById(R.id.loadingBar);
+        this.btnSubmit = this.findViewById(R.id.btnSubmit);
+    }
+
+    public void onClickRegister(View v) {
+        this.loadingBar.setVisibility(ProgressBar.VISIBLE);
+        this.btnSubmit.setEnabled(false);
+
+        String email = this.ptxtEmail.getText().toString();
+        String password = this.ptxtPassword.getText().toString();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this);
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+        }
     }
 }
