6d9d986c693c2e86236a509cf89c7203b7cf9f2a
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-15 10:18:08 -0300

    refactor layout for login activity

--- app/src/main/java/com/example/slatielly/LoginActivity.java
+++ app/src/main/java/com/example/slatielly/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.slatielly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickJoin(View v) {
+
+    }
+
+    public void onClickRegister(View v) {
+        
     }
 }
