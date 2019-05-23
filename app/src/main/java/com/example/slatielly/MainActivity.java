e46846625f3769284c55dd11df795bf01ca7f147
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-23 15:53:19 -0300

    add font awesome icons

--- app/src/main/java/com/example/slatielly/MainActivity.java
+++ app/src/main/java/com/example/slatielly/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.slatielly;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import info.androidhive.fontawesome.FontDrawable;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -19,34 +25,75 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private FragmentManager fragmentManager;
+    private int[] icons = {
+            R.string.fa_female_solid, R.string.fa_calendar_alt_solid
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.initNavView();
+        this.initDrawerLayout();
+
+        this.fragmentManager = this.getSupportFragmentManager();
+
+        MenuItem menuItem = this.navigationView.getMenu().getItem(0);
+        FontDrawable icon = (FontDrawable) menuItem.getIcon();
+        icon.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        menuItem.setChecked(true);
+
+        this.setNavigationFragment(R.id.fragment_content, new AllDressesFragment());
+    }
+
+    private void initDrawerLayout() {
         this.toolbar = this.findViewById(R.id.toolbar);
         this.setSupportActionBar(toolbar);
 
         this.drawerLayout = this.findViewById(R.id.drawerLayout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
-        this.drawerLayout.addDrawerListener(toggle);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, this.drawerLayout, this.toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+
+        this.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initNavView() {
         this.navigationView = this.findViewById(R.id.navView);
         this.navigationView.setNavigationItemSelectedListener(this);
 
-        toggle.syncState();
+        ImageView iconHeader = navigationView.getHeaderView(0).findViewById(R.id.nav_header_imageView);
+        FontDrawable drawable = new FontDrawable(this, R.string.fa_font_awesome, false, true);
+        drawable.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+        drawable.setTextSize(35);
+        iconHeader.setImageDrawable(drawable);
 
-        this.fragmentManager = this.getSupportFragmentManager();
+        this.renderMenuIcons(navigationView.getMenu(), true, false);
+    }
 
-        this.setNavigationFragment(this.navigationView.getMenu().getItem(0), R.id.fragment_content);
+    private void renderMenuIcons(Menu menu, boolean isSolid, boolean isBrand) {
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            if (!menuItem.hasSubMenu()) {
+                FontDrawable drawable = new FontDrawable(this, this.icons[i], isSolid, isBrand);
+                drawable.setTextColor(ContextCompat.getColor(this, R.color.colorGray600));
+                drawable.setTextSize(25);
+                menu.getItem(i).setIcon(drawable);
+            }
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setChecked(true);
+        FontDrawable icon = (FontDrawable) menuItem.getIcon();
+        icon.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
         switch (menuItem.getItemId()) {
             case R.id.nav_item_all_dresses: {
-                this.setNavigationFragment(menuItem, R.id.fragment_content);
+                this.setNavigationFragment(R.id.fragment_content, new AllDressesFragment());
                 break;
             }
 
@@ -74,11 +121,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    private void setNavigationFragment(@NonNull MenuItem menuItem, int id) {
+    private void setNavigationFragment(int id, Fragment fragment) {
         this.fragmentManager.beginTransaction()
-                .add(id, new AllDressesFragment())
+                .add(id, fragment)
                 .commit();
-
-        menuItem.setChecked(true);
     }
 }
