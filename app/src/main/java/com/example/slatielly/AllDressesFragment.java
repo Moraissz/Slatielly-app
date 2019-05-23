cb9dad915e570de9feffe07cf2036d80e765fa29
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-23 11:57:00 -0300

    add navigation drawer to main activity

---
+++ app/src/main/java/com/example/slatielly/AllDressesFragment.java
@@ -0,0 +1,103 @@
+package com.example.slatielly;
+
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.slatielly.Model.Dress;
+import com.example.slatielly.view.Dress.DressAdapter;
+
+import java.util.ArrayList;
+
+
+public class AllDressesFragment extends Fragment implements DressAdapter.DressListener {
+
+    private RecyclerView recyclerView;
+    private DressAdapter adapter;
+    private ArrayList<Dress> dressArrayList;
+
+    public AllDressesFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_all_dresses, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = this.getView().findViewById(R.id.recycler_view);
+        dressArrayList = new ArrayList<>();
+        //Preencher array
+        for (int i = 0; i < 20; i++) {
+            Dress dress = new Dress(i, "Vestido para Casamento", "Tipo " + i, "R$ 50,00");
+            dressArrayList.add(dress);
+        }
+        adapter = new DressAdapter(dressArrayList, this);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this.getActivity(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClickDressListener(Dress dress) {
+
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+}
--- app/src/main/java/com/example/slatielly/MainActivity.java
+++ app/src/main/java/com/example/slatielly/MainActivity.java
@@ -1,128 +1,78 @@
 package com.example.slatielly;
 
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Rect;
-import android.support.design.widget.AppBarLayout;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
-import android.view.View;
-import android.support.design.widget.CollapsingToolbarLayout;
-import com.example.slatielly.Model.Dress;
-import com.example.slatielly.view.Dress.DressAdapter;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity implements DressAdapter.DressListener {
-    private RecyclerView recyclerView;
-    private DressAdapter adapter;
-    private ArrayList<Dress> dressArrayList;
-
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
 
-        initCollapsingToolbar();
+        this.drawerLayout = (DrawerLayout) this.findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        this.drawerLayout.addDrawerListener(toggle);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        dressArrayList = new ArrayList<>();
-        //Preencher array
-        for(int i = 0; i< 20;i++){
-            Dress dress = new Dress(i,"Vestido para Casamento","Tipo "+i,"R$ 50,00");
-            dressArrayList.add(dress);
-        }
-        adapter = new DressAdapter(dressArrayList,this);
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
+        this.navigationView = (NavigationView) this.findViewById(R.id.navView);
+        this.navigationView.setNavigationItemSelectedListener(this);
 
+        toggle.syncState();
+
+        this.fragmentManager = this.getSupportFragmentManager();
+
+        this.fragmentManager.beginTransaction()
+                .add(R.id.fragment_content, new AllDressesFragment())
+                .commit();
     }
 
     @Override
-    public void onClickDressListener(Dress dress) {
-        
-    }
-
-    private void initCollapsingToolbar() {
-        final CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle(" ");
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
-        appBarLayout.setExpanded(true);
-
-        // hiding & showing the title when toolbar expanded & collapsed
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            boolean isShow = false;
-            int scrollRange = -1;
-
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                if (scrollRange == -1) {
-                    scrollRange = appBarLayout.getTotalScrollRange();
-                }
-                if (scrollRange + verticalOffset == 0) {
-                    collapsingToolbar.setTitle(getString(R.string.app_name));
-                    isShow = true;
-                } else if (isShow) {
-                    collapsingToolbar.setTitle(" ");
-                    isShow = false;
-                }
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_item_all_books: {
+                Toast.makeText(this, "Menu 1", Toast.LENGTH_SHORT).show();
+                break;
             }
-        });
-    }
-
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
+            case R.id.nav_item_all_dresses: {
+                this.fragmentManager.beginTransaction()
+                        .add(R.id.fragment_content, new AllDressesFragment())
+                        .commit();
+                break;
+            }
+            default: {
+                Toast.makeText(this, "Menu Default", Toast.LENGTH_SHORT).show();
+                break;
             }
         }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
-    /**
-     * Converting dp to pixel
-     */
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
