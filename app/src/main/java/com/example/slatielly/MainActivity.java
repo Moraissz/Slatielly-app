9db2eff019052a6235c7c7827c820b300c9939d9
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-05-24 11:33:30 -0300

    fix issues from merge

--- app/src/main/java/com/example/slatielly/MainActivity.java
+++ app/src/main/java/com/example/slatielly/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.slatielly;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
@@ -10,7 +10,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
